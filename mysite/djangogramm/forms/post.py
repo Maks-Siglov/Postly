b92a51589c0e694b20072938e3790e089e6136b6
Maks <gmb333x2@gmail.com>
2024-01-01 19:30:31 +0200

    Basic Tag logic provided

--- mysite/djangogramm/forms/post.py
+++ mysite/djangogramm/forms/post.py
@@ -4,6 +4,8 @@ from djangogramm.models import Post
 
 
 class PostForm(forms.ModelForm):
+    tag = forms.CharField(max_length=255, required=False)
+
     class Meta:
         model = Post
         fields = ['title', 'content', 'image']
--- mysite/djangogramm/views/post.py
+++ mysite/djangogramm/views/post.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 from djangogramm.forms import PostForm
-from djangogramm.models import Post
+from djangogramm.models import Post, Tag
 
 
 @login_required(login_url='login')
@@ -12,7 +12,14 @@ def create_post(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.owner = request.user
+
+            tag_name = form.cleaned_data['tag']
+            if tag_name:
+                tag, created = Tag.objects.get_or_create(name=tag_name)
+                post.tag = tag
+
             post.save()
+            print(post.tag.name)
             return redirect('post_list')
     else:
         form = PostForm()
