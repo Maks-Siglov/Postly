54216f427acae7bda9571462865a4ec06c47b3ab
Maks <gmb333x2@gmail.com>
2024-01-18 13:05:02 +0200

    Minor improvement

--- djangogramm/djangogramm/settings.py
+++ djangogramm/djangogramm/settings.py
@@ -78,7 +78,7 @@ ROOT_URLCONF = "djangogramm.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [BASE_DIR / "templates", BASE_DIR / "users/templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
