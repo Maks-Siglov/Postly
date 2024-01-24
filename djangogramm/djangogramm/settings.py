b861dbe2c7e5ec5aa4519101ba80c889fff462d2
Maks <gmb333x2@gmail.com>
2024-01-24 21:31:47 +0200

    Minor change

--- djangogramm/djangogramm/settings.py
+++ djangogramm/djangogramm/settings.py
@@ -71,6 +71,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
 
+    'django.contrib.sites.middleware.CurrentSiteMiddleware',
     "allauth.account.middleware.AccountMiddleware",
 ]
 
--- djangogramm/post/views.py
+++ djangogramm/post/views.py
@@ -1,3 +1,5 @@
+from django.contrib.sites.models import Site
+from django.contrib.sites.shortcuts import get_current_site
 from django.core.paginator import Paginator
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
