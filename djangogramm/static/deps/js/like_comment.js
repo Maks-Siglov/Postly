60db16a3f07a0545273521c19420495e07189b56
Maks-Siglov <gmb333x2@gmail.com>
2024-02-02 19:06:42 +0200

    Js improved

--- djangogramm/static/deps/js/like_comment.js
+++ djangogramm/static/deps/js/like_comment.js
@@ -1,43 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const likeButtons = document.querySelectorAll('.like-comment-button')
-    const dislikeButtons = document.querySelectorAll('.dislike-comment-button')
+    const likeButtons = document.querySelectorAll('.like-comment-button');
+    const dislikeButtons = document.querySelectorAll('.dislike-comment-button');
 
-    likeButtons.forEach(function (button){
-        button.addEventListener('click', function (event){
-            event.preventDefault();
-
-            const url = this.getAttribute('data-url');
-            const commentId = this.getAttribute('data-comment-id');
-
-            const csrftoken = getCookie('csrftoken');
-
-            const xhr = new XMLHttpRequest();
-            xhr.open('POST', url, true)
-            xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-            xhr.setRequestHeader('X-CSRFToken', csrftoken);
-
-            xhr.onreadystatechange = function (){
-                if (xhr.readyState === 4 && xhr.status === 200){
-                    const response = JSON.parse(xhr.responseText);
-                    const success = response.success
-                    const likeCount = response.like_count
-                    const dislikeCount = response.dislike_count
-
-                    if (success) {
-                        let likeCountSpan = document.getElementById('like-comment-count-'+commentId);
-                        likeCountSpan.textContent = likeCount;
-                        let dislikeCountSpan = document.getElementById('dislike-comment-count-'+commentId);
-                        dislikeCountSpan.textContent = dislikeCount;
-
-                    } else {
-                        console.error('Error liking the post.');
-                    }
-                }
-            };
-            xhr.send()
-        });
+    likeButtons.forEach(function (button) {
+        handleCommentButtonClick(button, readyXhr);
     });
-    dislikeButtons.forEach(function (button){
+
+    dislikeButtons.forEach(function (button) {
+        handleCommentButtonClick(button, readyXhr);
+    });
+
+    function handleCommentButtonClick(button, actionCallback) {
         button.addEventListener('click', function (event) {
             event.preventDefault();
 
@@ -47,31 +20,34 @@ document.addEventListener("DOMContentLoaded", function () {
             const csrftoken = getCookie('csrftoken');
 
             const xhr = new XMLHttpRequest();
-            xhr.open('POST', url, true)
+            xhr.open('POST', url, true);
             xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
             xhr.setRequestHeader('X-CSRFToken', csrftoken);
 
-            xhr.onreadystatechange = function (){
-                if (xhr.readyState === 4 && xhr.status === 200){
-                    const response = JSON.parse(xhr.responseText);
-                    const success = response.success
-                    const likeCount = response.like_count
-                    const dislikeCount = response.dislike_count
-
-                    if (success) {
-                        let likeCountSpan = document.getElementById('like-comment-count-'+commentId);
-                        likeCountSpan.textContent = likeCount;
-                        let dislikeCountSpan = document.getElementById('dislike-comment-count-'+commentId);
-                        dislikeCountSpan.textContent = dislikeCount;
-                    } else {
-                        console.error('Error disliking the post.');
-                    }
-                }
+            xhr.onreadystatechange = function () {
+                actionCallback(xhr, commentId);
             };
-            xhr.send()
-
+            xhr.send();
         });
-    });
+    }
+
+    function readyXhr(xhr, commentId) {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const response = JSON.parse(xhr.responseText);
+            const success = response.success;
+            const likeCount = response.like_count;
+            const dislikeCount = response.dislike_count;
+
+            if (success) {
+                let likeCountSpan = document.getElementById('like-comment-count-' + commentId);
+                likeCountSpan.textContent = likeCount;
+                let dislikeCountSpan = document.getElementById('dislike-comment-count-' + commentId);
+                dislikeCountSpan.textContent = dislikeCount;
+            } else {
+                console.error('Error disliking the post.');
+            }
+        }
+    }
 
     function getCookie(name) {
         const cookieValue = document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)');
--- djangogramm/static/deps/js/like_post.js
+++ djangogramm/static/deps/js/like_post.js
@@ -1,43 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const likeButtons = document.querySelectorAll('.like-button')
-    const dislikeButtons = document.querySelectorAll('.dislike-button')
+    const likeButtons = document.querySelectorAll('.like-button');
+    const dislikeButtons = document.querySelectorAll('.dislike-button');
 
-    likeButtons.forEach(function (button){
-        button.addEventListener('click', function (event){
-            event.preventDefault();
-
-            const url = this.getAttribute('data-url');
-            const postId = this.getAttribute('data-post-id');
-
-            const csrftoken = getCookie('csrftoken');
-
-            const xhr = new XMLHttpRequest();
-            xhr.open('POST', url, true)
-            xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-            xhr.setRequestHeader('X-CSRFToken', csrftoken);
-
-            xhr.onreadystatechange = function (){
-                if (xhr.readyState === 4 && xhr.status === 200){
-                    const response = JSON.parse(xhr.responseText);
-                    const success = response.success
-                    const likeCount = response.like_count
-                    const dislikeCount = response.dislike_count
-
-                    if (success) {
-                        let likeCountSpan = document.getElementById('like-count-'+postId);
-                        likeCountSpan.textContent = likeCount;
-                        let dislikeCountSpan = document.getElementById('dislike-count-'+postId);
-                        dislikeCountSpan.textContent = dislikeCount;
-
-                    } else {
-                        console.error('Error liking the post.');
-                    }
-                }
-            };
-            xhr.send()
-        });
+    likeButtons.forEach(function (button) {
+        handleButtonClick(button);
     });
-    dislikeButtons.forEach(function (button){
+
+    dislikeButtons.forEach(function (button) {
+        handleButtonClick(button);
+    });
+
+    function handleButtonClick(button, actionCallback) {
         button.addEventListener('click', function (event) {
             event.preventDefault();
 
@@ -47,31 +20,35 @@ document.addEventListener("DOMContentLoaded", function () {
             const csrftoken = getCookie('csrftoken');
 
             const xhr = new XMLHttpRequest();
-            xhr.open('POST', url, true)
+            xhr.open('POST', url, true);
             xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
             xhr.setRequestHeader('X-CSRFToken', csrftoken);
 
-            xhr.onreadystatechange = function (){
-                if (xhr.readyState === 4 && xhr.status === 200){
-                    const response = JSON.parse(xhr.responseText);
-                    const success = response.success
-                    const likeCount = response.like_count
-                    const dislikeCount = response.dislike_count
-
-                    if (success) {
-                        let likeCountSpan = document.getElementById('like-count-'+postId);
-                        likeCountSpan.textContent = likeCount;
-                        let dislikeCountSpan = document.getElementById('dislike-count-'+postId);
-                        dislikeCountSpan.textContent = dislikeCount;
-                    } else {
-                        console.error('Error disliking the post.');
-                    }
-                }
+            xhr.onreadystatechange = function () {
+                readyXhr(xhr, postId);
             };
-            xhr.send()
 
+            xhr.send();
         });
-    });
+    }
+
+    function readyXhr(xhr, postId) {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const response = JSON.parse(xhr.responseText);
+            const success = response.success;
+            const likeCount = response.like_count;
+            const dislikeCount = response.dislike_count;
+
+            if (success) {
+                let likeCountSpan = document.getElementById('like-count-' + postId);
+                likeCountSpan.textContent = likeCount;
+                let dislikeCountSpan = document.getElementById('dislike-count-' + postId);
+                dislikeCountSpan.textContent = dislikeCount;
+            } else {
+                console.error('Error disliking the post.');
+            }
+        }
+    }
 
     function getCookie(name) {
         const cookieValue = document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)');
